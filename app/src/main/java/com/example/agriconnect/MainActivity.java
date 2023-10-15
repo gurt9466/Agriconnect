c140c8c62ac64957bc3e3df32d1a6d8f505c9c74
gurt9466 <106596661+gurt9466@users.noreply.github.com>
2023-10-15 15:40:05 +0800

    updated

--- app/src/main/java/com/example/agriconnect/MainActivity.java
+++ app/src/main/java/com/example/agriconnect/MainActivity.java
@@ -3,34 +3,122 @@ package com.example.agriconnect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     TextView register;
-    EditText username,password;
+    EditText edtusername,edtpassword,edtemail;
     Button login;
+    String fname,email,password,apiKey;
+    TextView txtviewerror;
+    ProgressBar progressBar;
+    SharedPreferences sharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        username = findViewById(R.id.editTextlUsername);
-        password = findViewById(R.id.editTextlPassword);
-
+        //edtusername = findViewById(R.id.editTextlUsername);
+        edtemail = findViewById(R.id.editTextlUsername);
+        edtpassword = findViewById(R.id.editTextlPassword);
+        txtviewerror = findViewById(R.id.error);
+        progressBar = findViewById(R.id.loading);
         login = findViewById(R.id.loginbutton);
         register = findViewById(R.id.textViewclickhere);
+        sharedPreferences = getSharedPreferences("Agriconnect",MODE_PRIVATE);
+
+        if(sharedPreferences.getString("logged","false").equals("true")){
+            Intent intent = new Intent(getApplicationContext(),home.class);
+            startActivity(intent);
+            finish();
+
+        }
 
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    Intent intent = new Intent(MainActivity.this, home.class);
-                    startActivity(intent);
+                progressBar.setVisibility(View.VISIBLE);
+                txtviewerror.setVisibility(View.GONE);
+                email = String.valueOf(edtemail.getText());
+                //fname = String.valueOf(edtusername.getText());
+                password = String.valueOf(edtpassword.getText());
+
+
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url = "http://192.168.1.11/agriconnect/php/login.php"; //host ip and phpfile
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                progressBar.setVisibility(View.GONE);
+                                try {
+                                    JSONObject jsonObject = new JSONObject(response);
+                                    String status = jsonObject.getString("status");
+                                    String message = jsonObject.getString("message");
+                                    if(status.equals("success")){
+                                        fname = jsonObject.getString("name");
+                                        email = jsonObject.getString("email");
+                                        apiKey = jsonObject.getString("apiKey");
+                                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                                        editor.putString("logged","true");
+                                        editor.putString("name",fname);
+                                        editor.putString("email",email);
+                                        editor.putString("apiKey",apiKey);
+                                        editor.apply();
+                                        Intent intent = new Intent(getApplicationContext(),home.class);
+                                        startActivity(intent);
+                                        finish();
+
+                                    }else {
+                                     txtviewerror.setText(message);
+                                     txtviewerror.setVisibility(View.VISIBLE);
+                                    }
+                                } catch (JSONException e) {
+                                    throw new RuntimeException(e);
+
+                                }
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressBar.setVisibility(View.GONE);
+                        txtviewerror.setVisibility(View.VISIBLE);
+                        txtviewerror.setText(error.getLocalizedMessage());
+                    }
+                }){
+                    protected Map<String, String> getParams(){
+                        Map<String, String> paramV = new HashMap<>();
+                        paramV.put("email",email);
+                        paramV.put("password",password);
+                        return paramV;
+                    }
+                };
+                queue.add(stringRequest);
+
             }
         });
 
--- app/src/main/java/com/example/agriconnect/home.java
+++ app/src/main/java/com/example/agriconnect/home.java
@@ -2,13 +2,94 @@ package com.example.agriconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class home extends AppCompatActivity {
+    TextView txtname, txtemail, txtviewfetchreslt;
+    Button btnlogout, btnfetch;
+    SharedPreferences sharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        txtname = findViewById(R.id.txtviewname);
+        txtemail = findViewById(R.id.txtviewemail);
+        sharedPreferences = getSharedPreferences("Agriconnect", MODE_PRIVATE);
+        btnlogout = findViewById(R.id.loguot);
+        btnfetch = findViewById(R.id.fetch);
+        txtviewfetchreslt = findViewById(R.id.ftechresult);
+
+        if (sharedPreferences.getString("logged", "false").equals("false")) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+        txtname.setText(sharedPreferences.getString("name",""));
+        txtemail.setText(sharedPreferences.getString("email",""));
+
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url = "http://192.168.1.11/agriconnect/php/logout.php"; //host ip and phpfile
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                                if (response.equals("success")) {
+                                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                                    editor.putString("logged", "");
+                                    editor.putString("name", "");
+                                    editor.putString("email", "");
+                                    editor.putString("apiKey", "");
+                                    editor.apply();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                                else{
+                                    Toast.makeText(home.this, response, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }) {
+                    protected Map<String, String> getParams() {
+                        Map<String, String> paramV = new HashMap<>();
+                        paramV.put("email", sharedPreferences.getString("email",""));
+                        paramV.put("apiKey", sharedPreferences.getString("apiKey",""));
+                        return paramV;
+                    }
+                };
+                queue.add(stringRequest);
+
+            }
+        });
     }
 }
